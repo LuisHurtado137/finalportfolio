7e866e0d7c99102d8726f90431de9d56b6bf233c
LuisHurtado137 <78180223+LuisHurtado137@users.noreply.github.com>
2022-06-20 20:40:49 -0500

    local databese created

---
+++ src/components/database/dataBase.js
@@ -0,0 +1,74 @@
+export const projects = [
+  {
+    id: "1",
+    title: "Pale Blue Dot",
+    image:
+      "https://d33wubrfki0l68.cloudfront.net/60e651d33d335a0007692228/screenshot_2021-07-08-01-16-08-0000.png",
+    date: "03/13/2021",
+    tools: "HTML / CSS",
+    description:
+      "You can find a few podcast regarding technology and programming",
+    link: "https://podcastchannel1.netlify.app/",
+  },
+  {
+    id: "2",
+    title: "Hotel Reservation",
+    image:
+      "https://d33wubrfki0l68.cloudfront.net/60e652b751ff1e0007f85359/screenshot_2021-07-08-01-21-10-0000.png",
+    date: "06/13/2021",
+    tools: "HTML / CSS / JS",
+    description:
+      "Wherever you want to be, look at the hotels we have around the world and book one",
+    link: "https://hotel-vacation.netlify.app",
+  },
+  {
+    id: "3",
+    title: "Gifos",
+    image:
+      "https://d33wubrfki0l68.cloudfront.net/612127203aa06f2b77ae35bf/screenshot_2021-08-21-16-19-01-0000.png",
+    date: "08/29/2021",
+    tools: "HTML / CSS / ReactJS",
+    description:
+      "Trending? Spiderman? Cats? All the gifs you want can be found here",
+    link: "https://gifosacamica.netlify.app/",
+  },
+];
+
+export const skillsData = [
+  {
+    name: "JS",
+    imag: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+  },
+  {
+    name: "HTML",
+    imag: "https://cdn-icons-png.flaticon.com/512/732/732212.png?w=360",
+  },
+  {
+    name: "CSS",
+    imag: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/800px-CSS3_logo.svg.png",
+  },
+  {
+    name: "REACT",
+    imag: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png",
+  },
+  {
+    name: "STYLED-COMPONENTS",
+    imag: "https://styled-components.com/atom.png",
+  },
+  {
+    name: "GIT",
+    imag: "https://miro.medium.com/max/383/0*sC--lwQcHpzb7f2O.png",
+  },
+  {
+    name: "SELENIUM",
+    imag: "https://seeklogo.com/images/S/selenium-logo-DB9103D7CF-seeklogo.com.png",
+  },
+  {
+    name: "JAVA",
+    imag: "https://blog.desafiolatam.com/wp-content/uploads/2018/05/java-logo.png",
+  },
+  {
+    name: "REDUX",
+    imag: "https://nightdeveloper.net/wp-content/uploads/2018/01/logo.png",
+  },
+];
